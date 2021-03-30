b77953e3e83c5dbadc3bd792139968ce5ca27bf8
Marcelo Augusto Reis <marceloaugustodosreis@usp.br>
2021-03-29 21:55:45 -0300

    deploy-01

--- locusx/wsgi.py
+++ locusx/wsgi.py
@@ -10,10 +10,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling, MediaCling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'locusx.settings')
 
-application = Cling(MediaCling(get_wsgi_application()))
-
-
+application = get_wsgi_application()

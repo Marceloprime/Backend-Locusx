7df50f2aa166d1394904a47714f7bfaf669a4b03
Marcelo Augusto Reis <marceloaugustodosreis@usp.br>
2021-04-02 00:50:23 -0300

    requests of location

--- content/api/viewsets.py
+++ content/api/viewsets.py
@@ -11,61 +11,61 @@ from .permissions import *
 
 
 class ContentViewSet(viewsets.ModelViewSet):
-  permission_classes = permissions.IsAuthenticated
+  permission_classes = [permissions.IsAuthenticated]
   serializer_class = ContentSerializers
   queryset = Content.objects.all()
 
 class QuestionViewSet(viewsets.ModelViewSet):
-  permission_classes = permissions.IsAuthenticated
+  permission_classes = [permissions.IsAuthenticated]
   serializer_class = QuestionSerializers
   queryset = Question.objects.all()
 
 class OpenQuestionViewSet(viewsets.ModelViewSet):
-  permission_classes = permissions.IsAuthenticated
+  permission_classes = [permissions.IsAuthenticated]
   serializer_class = OpenQuestionSerializers
   queryset = OpenQuestion.objects.all()
 
 class AlternativeViewSet(viewsets.ModelViewSet):
-  permission_classes = permissions.IsAuthenticated
+  permission_classes = [permissions.IsAuthenticated]
   serializer_class = AlternativeSerializers
   queryset = Alternative.objects.all()
 
 class MultipleChoiceQuestionViewSet(viewsets.ModelViewSet):
-  permission_classes = permissions.IsAuthenticated
+  permission_classes = [permissions.IsAuthenticated]
   serializer_class = MultipleChoiceQuestionSerializers
   queryset = MultipleChoiceQuestion.objects.all()
 
 class TaskViewSet(viewsets.ModelViewSet):
-  permission_classes = permissions.IsAuthenticated
+  permission_classes = [permissions.IsAuthenticated]
   serializer_class = TaskSerializers
   queryset = Task.objects.all()
 
 class ActivityTeacherViewSet(viewsets.ModelViewSet):
-  permission_classes = permissions.IsAuthenticated
+  permission_classes = [permissions.IsAuthenticated]
   serializer_class = ActivityTeacherSerializers
   queryset = ActivityTeacher.objects.all()
 
 class ActivityViewSet(viewsets.ModelViewSet):
-  permission_classes = permissions.IsAuthenticated
+  permission_classes = [permissions.IsAuthenticated]
   serializer_class = ActivitySerializers
   queryset = Activity.objects.all()
 
 class ActivityRealizationViewSet(viewsets.ModelViewSet):
-  permission_classes = permissions.IsAuthenticated
+  permission_classes = [permissions.IsAuthenticated]
   serializer_class = ActivityRealizationSerializers
   queryset = ActivityRealization.objects.all()
 
 class ActivityRealizationTeacherViewSet(viewsets.ModelViewSet):
-  permission_classes = permissions.IsAuthenticated
+  permission_classes = [permissions.IsAuthenticated]
   serializer_class = ActivityRealizationTeacherSerializers
   queryset = ActivityRealizationTeacher.objects.all()
 
 class AnswerViewSet(viewsets.ModelViewSet):
-  permission_classes = permissions.IsAuthenticated
+  permission_classes = [permissions.IsAuthenticated]
   serializer_class = AnswerSerializers
   queryset = Answer.objects.all()
 
 class AnswerTeacherViewSet(viewsets.ModelViewSet):
-  permission_classes = permissions.IsAuthenticated
+  permission_classes = [permissions.IsAuthenticated]
   serializer_class = AnswerTeacherSerializers
   queryset = AnswerTeacher.objects.all()

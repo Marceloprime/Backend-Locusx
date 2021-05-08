d936decd9b3cdffc7cd1d4cafbb4ea492f964005
Marceloprime <marceloreis7777@gmail.com>
2021-05-08 02:46:14 -0300

    correcao em locations

--- location/api/viewsets.py
+++ location/api/viewsets.py
@@ -21,19 +21,4 @@ class LocationViewSet(viewsets.ModelViewSet):
     else:
       return Location.objects.none()
 
-  def create(self, request):
-    getUser = self.request.user
-    getteacher = Teacher.objects.filter(user=getUser).values_list('id', flat = True)[0]
-    ObjLocation = Location()
-    ObjLocation.name = request.data.get('name')
-    ObjLocation.teacher_id = getteacher
-    ObjLocation.latitude = request.data.get('latitude')
-    ObjLocation.longitude = request.data.get('longitude')
-    ObjLocation.save()
-    
-    responseJson = {
-      "name" : request.data.get('name'),
-      "latitude" : request.data.get('latitude'),
-      "longitude" : request.data.get('longitude')
-    }
     return Response(responseJson)

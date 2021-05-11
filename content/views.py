4b139dd0cbdab1a82af424cdd6c89db8f33073ff
Marceloprime <marceloreis7777@gmail.com>
2021-05-11 16:58:11 -0300

    web

--- content/views.py
+++ content/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render , redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import *
 from .forms import *
 

fe43e5aae732265898fa7b421f42e70347e3c6d9
Marceloprime <marceloreis7777@gmail.com>
2021-05-15 14:32:02 -0300

    teste

--- location/views.py
+++ location/views.py
@@ -2,28 +2,34 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from accounts.models import Teacher
 from .models import *
 from .forms import *
 
+
 @login_required
 def LocationView(request):
-    getUser = request.user
-    getteacher = Teacher.objects.filter(user=getUser).values_list('id', flat = True)[0]
-    locations = Location.objects.filter(teacher=getteacher)
-    print(locations)
-    if str(request.method) == 'POST':
-        form = LocationModelForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Localização salvo com sucesso')
-            form = LocationModelForm()
-        else:
-            messages.error(request, 'Erro ao salvar localização')
-    else:
-        form = LocationModelForm()
+    if request.POST:
+        user = request.user
+        teacher = Teacher.objects.filter(user=user)[0]
 
-    context = {
-        'form': form,
-        'locations': locations
-    }
-    return render(request, 'location/Location.html',context)
+        name = request.POST['name']
+        description = request.POST['description']
+        latitude = request.POST['latitude']
+        longitude = request.POST['longitude']
+ 
+
+        if name == '' or description == '' or latitude == '' or longitude == '':
+            messages.error(request,"Campos inválidos")
+        else:
+            try:
+                Location.objects.create(name=name,teacher=teacher,description=description,latitude=float(latitude),longitude=float(longitude))
+                messages.success(request,"Localização criada com sucesso")
+            except:
+                messages.error(request,"Campos inválidos")
+
+        return render(request, 'location/Location.html')
+    else:
+        return render(request, 'location/Location.html')
+
+
--- locusx/settings.py
+++ locusx/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '%8+o(43h9r=-_5-#zmv^0)y)fqov6au50b@l&%xjsakp@bc*t6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 

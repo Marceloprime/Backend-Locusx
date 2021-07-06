88f85cbaa2607053fc97df6ccbeb2539ae10c47b
Marceloprime <marceloreis7777@gmail.com>
2021-07-06 03:55:37 -0300

    tela de login

--- locusx/settings.py
+++ locusx/settings.py
@@ -104,12 +104,12 @@ DATABASES = {
 #Para Teste
 """
 """
-
 #Para Produção - 
 default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 DATABASES = {
     'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
 }
+
 
 
 # Password validation
